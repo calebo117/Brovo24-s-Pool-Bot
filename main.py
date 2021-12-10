7234ea5519f7fe88d772bc03374d1b9e41ea5400
Caleb O <red_caleb2012@hotmail.com>
2021-12-10 16:34:23 +0000

    another basic commit

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-#adapted from freeCodeCamp.org youtube video: https://youtu.be/SPTfmiYiuok
+#initial build adapted from freeCodeCamp.org youtube video: https://youtu.be/SPTfmiYiuok
+
+#Description/Goal: a discord bot that will allow users to create and manage a 'betting' pool. Users will be able to create new pools with a starting value and then other users will be able to add a certain amount of value to the pool. At the end of the event, the pool will pay out a value to the winning user, which is agreed upon by the pool manager/creator and the entrants. 
 
 import os
 from replit import db
-from Pool.py import pool
+from Pool import pool
 import discord
 
 bot_token = os.environ['botToken']
@@ -20,15 +22,31 @@ async def on_ready():
 async def on_message(msg):
   if msg.author == client.user:
     return
+
   if msg.content.startswith('$hello'):
     await msg.channel.send('Hello')
+
   if msg.content.startswith('$newPool'):
-    # Extract the parts of the command
+    #TODO: Extract the parts of the command
     name = 'pool'
     value = '100'
-    #create new pool and add to DB
+    #TODO: create new pool and add to DB
+    #TODO: If pool name already exists, reject
     newPool = pool(name, value)
     poolList.append(newPool)
+    await msg.channel.send('Created: ' + name + ':' + value)
+  
+  #return the list of active pools to the user
+  if msg.content.startswith('$poolList'):
+    poolListString = ''
+    for x in poolList:
+      poolListString += ' ' + x.getName() + ':' + x.getValue() + ","
+      print(poolListString)
+    if poolListString:
+      await msg.channel.send(poolListString)
+      return
+    else:
+      await msg.channel.send('No active pools available.')
 
     
 
